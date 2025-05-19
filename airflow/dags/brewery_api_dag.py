a3f4ba4d0bc7d26c93c9e55c9dff095a8e1d8445
Glícia Oliveira <annagliciaol@gmail.com>
2025-05-19 18:32:07 -0300

    finished

--- airflow/dags/brewery_api_dag.py
+++ airflow/dags/brewery_api_dag.py
@@ -8,7 +8,6 @@ import pandas as pd
 
 bronze_layer_json_path = '/opt/airflow/db/bronze_layer_data.json'
 silver_layer_parquet_path = '/opt/airflow/db/silver_layer_data.parquet'
-gold_layer_parquet_path = '/opt/airflow/db/gold_layer_data.parquet'
 
 def read_json(json_path): 
       with open(json_path, 'r') as file:
@@ -23,14 +22,34 @@ def save_raw_data(task_instance):
 def first_transformation_data(task_instance):
         data = read_json(bronze_layer_json_path)
         df = pd.DataFrame(data[0][0])
-        df.to_parquet(path=silver_layer_parquet_path, partition_cols=['city'])
+        filtered_df = df.query('country == "United States"')
+        filtered_df.to_parquet(path=silver_layer_parquet_path, partition_cols=['city'])
                  
-def gold_layer_transformation(task_instance):
+def city_gold_layer_transformation(task_instance):
         df = pd.read_parquet(silver_layer_parquet_path).drop_duplicates()
-        grouped_df = df.groupby(['city', 'brewery_type']).count()
-        filter_df = grouped_df[grouped_df["id"] > 0]
-        final_df = filter_df.iloc[:, 0:1].rename(columns={'id': 'count'})
-        final_df.to_parquet(path=gold_layer_parquet_path)
+        
+        city_grouped_df = df.groupby(['city', 'brewery_type']).count()
+        city_filter_df = city_grouped_df[city_grouped_df["id"] > 0]
+        city_final_df = city_filter_df.iloc[:, 0:1].rename(columns={'id': 'count'})
+        city_final_df.to_parquet(path='/opt/airflow/db/gold_layer_data/city_type_grouped_data.parquet')
+
+def state_gold_layer_transformation(task_instance):
+        df = pd.read_parquet(silver_layer_parquet_path).drop_duplicates()
+        
+        state_grouped_df = df.groupby(['state_province']).count()
+        state_filter_df = state_grouped_df[state_grouped_df["id"] > 0]
+        state_final_df = state_filter_df.iloc[:, 0:1].rename(columns={'id': 'count'})
+        state_final_df.to_parquet(path='/opt/airflow/db/gold_layer_data/state_grouped_data.parquet')
+
+def type_gold_layer_transformation(task_instance):
+        df = pd.read_parquet(silver_layer_parquet_path).drop_duplicates()
+        
+        type_grouped_df = df.groupby(['brewery_type']).count()
+        type_filter_df = type_grouped_df[type_grouped_df["id"] > 0]
+        type_final_df = type_filter_df.iloc[:, 0:1].rename(columns={'id': 'count'})
+        type_final_df.to_parquet(path='/opt/airflow/db/gold_layer_data/type_grouped_data.parquet')
+
+
 
 default_args = {
     'owner': 'airflow',
@@ -75,10 +94,22 @@ with DAG("brewery_dag",
 
         )
 
-        gold_layer = PythonOperator(
-               task_id = 'gold_layer',
-               python_callable = gold_layer_transformation
+        gold_layer_city_type = PythonOperator(
+               task_id = 'gold_layer_city_type',
+               python_callable = city_gold_layer_transformation
 
         )
 
-        is_api_ready >> extract_brewery_data >> bronze_layer >> silver_layer >> gold_layer
+        gold_layer_state_type = PythonOperator(
+               task_id = 'gold_layer_state_type',
+               python_callable = state_gold_layer_transformation
+
+        )
+
+        gold_layer_type = PythonOperator(
+               task_id = 'gold_layer_type',
+               python_callable = type_gold_layer_transformation
+
+        )
+
+        is_api_ready >> extract_brewery_data >> bronze_layer >> silver_layer >> gold_layer_city_type >> gold_layer_state_type >> gold_layer_type
--- dataviz.py
+++ dataviz.py
@@ -1,7 +1,27 @@
 import pandas as pd
 import streamlit as st
 
-gold_layer_parquet_path = './airflow/db/gold_layer_data.parquet'
+bronze_layer_parquet_path = './airflow/db/bronze_layer_data.json'
 
-df_gold = pd.read_parquet(gold_layer_parquet_path)
-st.write(df_gold)
+st.title("Brewery Report in the United States")
+
+st.write("The data for this report was extracted " \
+        "from the Open Brewery DB API. Below are the raw " \
+        "data provided by the API, filtered to the United States.")
+
+df_silver = pd.read_parquet('./airflow/db/silver_layer_data.parquet')
+st.dataframe(df_silver)
+
+st.subheader("Data grouped by city and brewery type")
+df_gold_city = pd.read_parquet('./airflow/db/gold_layer_data/city_type_grouped_data.parquet')
+st.dataframe(df_gold_city)
+
+st.subheader("Number of breweries by state")
+df_gold_state = pd.read_parquet('./airflow/db/gold_layer_data/state_grouped_data.parquet')
+st.dataframe(df_gold_state)
+st.bar_chart(df_gold_state)
+
+st.subheader("Types of breweries in the country")
+df_gold_type = pd.read_parquet('./airflow/db/gold_layer_data/type_grouped_data.parquet')
+st.dataframe(df_gold_type)
+st.bar_chart(df_gold_type)
